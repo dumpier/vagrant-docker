9cdf84b22db55e5b5202901d491594ed6945d313
dumpier <dumpieer@gmail.com>
2023-09-10 08:18:00 +0900

    #1 nodejs(chat)

--- docker/code/node/server.js
+++ docker/code/node/server.js
@@ -14,7 +14,8 @@ const data = {
   msgs:[],
 };
 
-io.on('connection', function(socket){
+
+io.on('connection', (socket)=>{
   console.log(`a user(${socket.id}) connected.`);
   if (!data.users.includes(socket.id)) {
     data.users.push(socket.id);
@@ -26,13 +27,13 @@ io.on('connection', function(socket){
 
   socket.on('chat message', function(msg){
     console.log('# message', msg);
-    const chat = { id: socket.id, msg:msg, time:new Date().toLocaleString('sv') };
+    const chat = { id: socket.id, msg:msg, time:new Date().toLocaleString('sv'), };
 
     data.msgs.push(chat);
     io.emit('chat message', chat);
   });
 
-  socket.on("disconnect",(reason)=>{
+  socket.on("disconnect",()=>{
     console.log("a user(${socket.id}) disconnect.");
     data.users = data.users.filter((user)=>{ return user!=socket.id });
     io.emit('chat user', data.users);
