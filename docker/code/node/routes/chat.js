bcc272f785ccb14c102436e8ea8d0b2bcdc565e6
dumpier <dumpieer@gmail.com>
2023-09-09 16:16:11 +0900

    nodejs(chat)

--- docker/code/node/routes/chat.js
+++ docker/code/node/routes/chat.js
@@ -1,8 +1,5 @@
 const path = require('path');
-const crypto = require('crypto');
 const express = require('express');
-const app = express();
-const http = require('http').Server(app);
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
--- docker/code/node/server.js
+++ docker/code/node/server.js
@@ -5,35 +5,40 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 
-// app.use("/", require(__dirname + '/routes/index'));
-// app.use("/chat", require(__dirname + '/routes/chat'));
-// app.use("/test", require(__dirname + '/routes/index'));
+app.use("/", require(__dirname + '/routes/index'));
+app.use("/chat", require(__dirname + '/routes/chat'));
 
-const users = [];
-const messages = [];
 
-app.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '/views/chat.html'));
-});
+const data = {
+  users:[],
+  msgs:[],
+};
 
 io.on('connection', function(socket){
   console.log(`a user(${socket.id}) connected.`);
-  if (!users.includes(socket.id)) {
-    users.push(socket.id);
-    io.emit('chat user', users);
+  if (!data.users.includes(socket.id)) {
+    data.users.push(socket.id);
+    io.emit('chat user', data.users);
   }
-  if (messages.length) {
-    io.to(socket.id).emit('chat message', messages);
+  if (data.msgs.length) {
+    io.to(socket.id).emit('chat message', data.msgs);
   }
 
   socket.on('chat message', function(msg){
     console.log('# message', msg);
-    const data = { id: socket.id, msg:msg, time:new Date().toLocaleString('sv') };
+    const chat = { id: socket.id, msg:msg, time:new Date().toLocaleString('sv') };
 
-    messages.push(data);
-    io.emit('chat message', data);
+    data.msgs.push(chat);
+    io.emit('chat message', chat);
+  });
+
+  socket.on("disconnect",(reason)=>{
+    console.log("a user(${socket.id}) disconnect.");
+    data.users = data.users.filter((user)=>{ return user!=socket.id });
+    io.emit('chat user', data.users);
   });
 });
+
 
 
 http.listen(3000, ()=>{ console.log('listening on *:3000'); });
